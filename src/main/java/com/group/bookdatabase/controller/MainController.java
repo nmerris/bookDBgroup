aa504aa3be1c7397c8f2487b94ba18f7390bc77d
nmerris <nate.merris@gmail.com>
2017-08-03 16:57:06 -0400

    addbook page with views

--- src/main/java/com/group/bookdatabase/controller/MainController.java
+++ src/main/java/com/group/bookdatabase/controller/MainController.java
@@ -4,6 +4,7 @@ import javax.persistence.Id;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.group.bookdatabase.model.Book;
 
 @Controller
 public class MainController
@@ -15,5 +16,27 @@ public class MainController
         model.addAttribute("welcomemessage","Welcome to this book DB");
         return "index";
     }
+
+
+    @GetMapping("/addbook")
+    public String addBookPage(Model model)
+    {
+        model.addAttribute("newbook", new Book());
+
+        model.addAttribute("addbookmessage","please add book");
+        return "addbook";
+    }
+
+
+    // this method is called when the user clicks on the LoadBooks link from the index page
+    // there is no new page to go to, all it does is load the db with a list of predefined books
+    // all this happens righ here
+    @GetMapping("/loadbooks")
+    public void addBookPage()
+    {
+
+    }
+
+
 
 }
